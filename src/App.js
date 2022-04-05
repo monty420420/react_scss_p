a07968239a7f4ac54484c9e93dea3ddd2378631d
Muntaeyeon <monty420420@naver.com>
2022-04-05 15:16:52 +0900

    scss_variable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-           <h1>scss practice</h1>
+          <Navbar />
     </div>
   );
 }
